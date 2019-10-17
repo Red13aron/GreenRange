88130a2f0f87fd40880469f737617322f1eb3018
Elianice Gorniak <eligorniak@gmail.com>
2019-10-17 14:20:04 -0500

    updated Header component

--- green-range/src/App.js
+++ green-range/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Wrapper>
+      <Navbar></Navbar>
       <Header></Header>
       <Footer />
     </Wrapper>
